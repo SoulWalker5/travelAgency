af73130986c0deec7b451927f3e18dd3e1b9c0c3
SoulWalker5 <shrituon@gmail.com>
2023-06-13 22:46:42 +0300

    added DB structure and factories

---
+++ app/Models/Tour.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tour extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'travelId',
+        'name',
+        'startingDate',
+        'endingDate',
+        'price',
+    ];
+
+    protected $casts = [
+        'startingDate' => 'datetime',
+        'endingDate' => 'datetime',
+    ];
+}
--- database/migrations/2023_06_13_192648_create_travel_table.php
+++ database/migrations/2023_06_13_192648_create_travel_table.php
@@ -11,13 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('travel', function (Blueprint $table) {
             $table->uuid();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('emailVerifiedAt')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('slug')->unique();
+            $table->string('description')->nullable();
+            $table->tinyInteger('numberOfDays');
+            $table->tinyInteger('numberOfNights');
+            $table->boolean('isPublic');
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('travel');
     }
 };
