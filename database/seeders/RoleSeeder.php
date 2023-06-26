6dcc430480b6e3cac9882c071f27d92e51011024
SoulWalker5 <shrituon@gmail.com>
2023-06-26 13:33:02 +0300

    update initial role and user seeders

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -12,6 +12,6 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        Role::factory()->createMany([['name' => Role::ADMIN], ['name' => Role::USER]]);
+        Role::factory()->createMany([['name' => Role::ADMIN], ['name' => Role::EDITOR]]);
     }
 }
